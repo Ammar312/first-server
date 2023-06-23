3f350ddc46aa0d008911c1caf4aab7d42fa1b739
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2023-06-23 18:46:44 +0500

    made a server

---
+++ public/script.js
@@ -0,0 +1,107 @@
+const currentWeather = (e) => {
+  e.preventDefault();
+  let cityName = document.querySelector("#cityInput").value;
+
+  // Recieving weather data through api
+  axios
+    .get(`/weather/${cityName}`)
+    .then(function (response) {
+      //   if (response.status === 404) {
+      //     document.querySelector(".welcomeAnderror").style.display = "block";
+      //     document.querySelector(".welcomeAnderror").innerHTML = "Invalid Input";
+      //   } else {
+      // const weatherCondition = response.data.weather[0].main;
+      // // Capitilize first letter of description
+      // const firstLetter = word.charAt(0);
+      // const firstLetterCap = firstLetter.toUpperCase();
+      // const remainingLetters = word.slice(1);
+      // const capitalizedWord = firstLetterCap + remainingLetters;
+      // Sending data to HTML
+      // document.querySelector(".description").innerHTML = `${capitalizedWord}`;
+      // document.querySelector("#openWeatherIcon").innerHTML = openWeatherIcon;
+      // document.querySelector(".feelsLike").innerHTML =
+      //   "Feels like " + Math.round(response.data.main.feels_like) + "°";
+      document.querySelector(".temperature").innerHTML =
+        Math.round(response.data.temp) + "°C";
+      document.querySelector(".cityName").innerHTML = response.data.name;
+      document.querySelector("#humidityPercent").innerHTML =
+        response.data.humidity + "%";
+      document.querySelector("#windPercent").innerHTML =
+        response.data.wind + " km/h";
+      document.querySelector("#visibilityValue").innerHTML =
+        response.data.visibility + " km/h";
+      cityName = "";
+      // Changing weather images dynamically
+      // if (weatherCondition === "Clear") {
+      //   weatherIcon.src = "images/clear.png";
+      //   audio.src = "clear.mp3";
+      //   document.querySelector("body").style.background =
+      //     "linear-gradient(to right bottom, #83a4d4, #b6fbff)";
+      // } else if (weatherCondition === "Clouds") {
+      //   audio.src = "clouds.wav";
+      //   weatherIcon.src = "images/clouds.png";
+      //   document.querySelector("body").style.background =
+      //     //   "linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(./rain2.jpg)";
+      //     // document.querySelector("body").style.backgroundSize = "cover";
+
+      //     "linear-gradient(to right bottom, #667db6, #485563, #0082c8, #667db6)";
+      // } else if (weatherCondition === "Drizzle") {
+      //   audio.src = "drizzle.mp3";
+      //   weatherIcon.src = "images/drizzle.png";
+      //   document.querySelector("body").style.background =
+      //     "linear-gradient(to right bottom, #4B79A1, #283E51)";
+      // } else if (weatherCondition === "Mist" || weatherCondition === "Haze") {
+      //   audio.src = "wind.wav";
+      //   weatherIcon.src = "images/mist.png";
+      //   document.querySelector("body").style.background =
+      //     "linear-gradient(to right bottom, #525252, #3d72b4)";
+      // } else if (weatherCondition === "Fog") {
+      //   audio.src = "fog.wav";
+      //   weatherIcon.src = "images/fog.png";
+      //   document.querySelector("body").style.background =
+      //     "linear-gradient(to right bottom, #bdc3c7, #2c3e50)";
+      // } else if (weatherCondition === "Thunderstorm") {
+      //   weatherIcon.src = "images/storm.png";
+      //   audio.src = "thunderwithrain.wav";
+      //   document.querySelector("body").style.background =
+      //     "linear-gradient(to right bottom, #232526, #414345)";
+      // } else if (
+      //   weatherCondition === "Dust" ||
+      //   weatherCondition === "Smoke" ||
+      //   weatherCondition === "Sand"
+      // ) {
+      //   audio.src = "wind.wav";
+      //   weatherIcon.src = "images/smokey1.png";
+      //   document.querySelector("body").style.background =
+      //     "linear-gradient(to right bottom, #FFA17F, #00223E)";
+      //   // document.querySelector("body").style.background =
+      //   //   "linear-gradient(to right bottom, #1e130c, #9a8478)";
+      // } else if (weatherCondition === "Rain") {
+      //   audio.src = "drizzle.mp3";
+      //   weatherIcon.src = "images/rain.png";
+      //   document.querySelector("body").style.background =
+      //     "linear-gradient(to right bottom, #093028, #237A57)";
+      // } else if (weatherCondition === "Snow") {
+      //   weatherIcon.src = "images/snow.png";
+      //   audio.src = "wind.wav";
+      //   document.querySelector("body").style.background =
+      //     "linear-gradient(to right bottom, #2c3e50, #3498db)";
+      // } else {
+      //   console.log("error hai");
+      // }
+      // // Display weather content
+      // document.querySelector(".weatherContent").style.display = "block";
+      // Hidden welcome and error display
+
+      // Changing body background color
+
+      console.log(response.data);
+      //   }
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error.data);
+    });
+};
+const form = document.querySelector(".form");
+form.addEventListener("submit", currentWeather);
--- server.mjs
+++ server.mjs
@@ -1,10 +1,37 @@
 import express from "express";
+import cors from "cors";
+import path from "path";
+const __dirname = path.resolve();
 const app = express();
-
+// app.use(cors());
 // 192.168.0.102:3000
-app.get("/", (req, res) => {
-  res.send("Hello World!" + " Ammar Here!");
+app.get("/weather/:cityName", (req, res) => {
+  let weatherData = {
+    karachi: {
+      name: "karachi",
+      temp: 30,
+      humidity: 56,
+      wind: 8,
+      visibility: 5,
+    },
+    london: {
+      name: "london",
+      temp: 5,
+      humidity: 56,
+      wind: 6,
+      visibility: 2,
+    },
+  };
+  const userInputCityName = req.params.cityName.toLowerCase();
+  const dataSend = weatherData[userInputCityName];
+  if (dataSend) {
+    res.send(dataSend);
+  } else {
+    res.status(404).send();
+  }
 });
+app.use(express.static(path.join(__dirname, "public")));
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Example app listening on PORT ${PORT}`);
